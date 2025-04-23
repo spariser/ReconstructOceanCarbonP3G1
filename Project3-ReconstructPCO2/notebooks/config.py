33c353c718bc2bd4514b7fb5cdc77742eefea06d
azamkhan99 <azan_khan99@outlook.com>
2025-04-23 18:19:40 -0400

    Added file_paths.json to define paths

---
+++ Project3-ReconstructPCO2/notebooks/config.py
@@ -0,0 +1,7 @@
+zarr_dir = "gs://leap-persistent/abbysh/zarr_files_"
+
+path_seeds = (
+    "gs://leap-persistent/abbysh/pickles/random_seeds.npy"  # random seeds for ML
+)
+grid_search_approach = "nmse"
+MLinputs_path = "gs://leap-persistent/Mukkke/pco2_residual/post01_xgb_inputs"
